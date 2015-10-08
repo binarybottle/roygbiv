e3e7c5da7550fadfcf39c9632e6c4cb6e399e390
Ben Cipollini <bcipolli@ucsd.edu>
2015-10-08 13:48:26 -0700

    Make server run.

--- server.py
+++ server.py
@@ -4,15 +4,24 @@ from flask import Flask, request, send_from_directory
 
 
 # set the project root directory as the static folder, you can set others.
-app = Flask(__name__)
+def launch_server(web_dir='web', data_dir='web/data', debug=False):
+    app = Flask(__name__)
 
-@app.route('/')
-def default_page():
-    return send_from_directory('web', 'index.html')
+    @app.route('/')
+    def default_page():
+        return send_from_directory(web_dir, 'index.html')
 
-@app.route('/<path:path>')
-def send_all(path):
-    return send_from_directory('web', path)
+    @app.route('/data/<path:path>')
+    def send_data(path):
+        return send_from_directory(data_dir, path)
+
+    @app.route('/<path:path>')
+    def send_all(path):
+        return send_from_directory(web_dir, path)
+
+    app.debug = debug
+    app.run()
+
 
 if __name__ == "__main__":
     import threading
@@ -20,5 +29,4 @@ if __name__ == "__main__":
 
     threading.Timer(1.25, lambda: webbrowser.open('http://127.0.0.1:5000/')).start()
 
-    app.debug = True
-    app.run()
+    launch_server(debug=True)
